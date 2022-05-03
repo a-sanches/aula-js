0d5a9fe0a648e214513b0c45b95df4e42ed9a808
Andressa Sanches <andressasanchesadvocacia@gmail.com>
2022-05-03 18:26:16 -0300

    commit dos exercícios

---
+++ filtra-pares.js
@@ -0,0 +1,11 @@
+function filtraPares(Arr) {
+    return Arr.filter (callback)
+}
+
+function callback(item){
+    return item % 2 === 0;
+}
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtraPares(meuArray));
---
+++ reduce.js
@@ -0,0 +1,7 @@
+function somaNumeros(arr) {
+    arr.reduce(function(prev, current){
+        return prev + current;
+    }) 
+}
+
+const arr = [1,2];
